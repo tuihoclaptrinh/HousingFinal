9ecb07ad6063218dcfd8b24e60fd353a031902bd
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2023-03-17 00:11:14 +0700

    Add All.

--- housing-front-end/src/app/services/housing.service.ts
+++ housing-front-end/src/app/services/housing.service.ts
@@ -11,18 +11,21 @@ export class HousingService {
 
   constructor(private http: HttpClient) { }
 
-  getAllProperties() : Observable<IProperty[]> {
+  getAllProperties(SellRent: number): Observable<IProperty[]> {
     return this.http.get('data/properties.json').pipe(
       map(data => {
         const propertiesArray: Array<IProperty> = [];
+
         for (const id in data) {
-          if (data.hasOwnProperty(id)) {
+          if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
             propertiesArray.push(data[id]);
           }
         }
         return propertiesArray;
       })
     );
+
+    return this.http.get<IProperty[]>('data/properties.json');
   }
 
 }
