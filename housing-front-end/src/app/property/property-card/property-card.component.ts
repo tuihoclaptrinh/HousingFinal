0082421fd09abc2caf23cd5d7a9b35f22c2ef3d9
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2023-03-16 22:08:25 +0700

    Passing data between components - Using ngForDirective.

--- housing-front-end/src/app/property/property-card/property-card.component.ts
+++ housing-front-end/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,12 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Property: any = {
-    "Id": 1,
-    "Name": "Mer House",
-    "Type": "House",
-    "Price": 12000
-  }
+  @Input() property: any;
 
   constructor() { }
 
