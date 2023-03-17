f3c4878f3a2809e7fe0a129692dfb48b1d3e14d8
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2023-03-17 10:59:34 +0700

    Implement add and delete operation in WebAPI.

--- housing-back-end/Controllers/CityController.cs
+++ housing-back-end/Controllers/CityController.cs
@@ -1,5 +1,7 @@
 ﻿using housing_back_end.Data;
+using housing_back_end.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace housing_back_end.Controllers;
 
@@ -16,9 +18,41 @@ public class CityController : ControllerBase
     }
     
     [HttpGet]
-    public IActionResult GetCities()
+    public async Task<IActionResult> GetCities()
     {
-        var cities = context.Cities.ToList();
+        var cities = await context.Cities.ToListAsync();
         return Ok(cities);
     }
+    
+    // Post api/city/add?cityName=Name
+    // Post api/city/add/Name
+    [HttpPost("add")]
+    [HttpPost("add/{cityName}")]
+    public async Task<IActionResult> AddCity(string cityName)
+    {
+        City city = new City();
+        city.Name = cityName;
+        await context.Cities.AddAsync(city);
+        await context.SaveChangesAsync();
+        return Ok(city);
+    }
+    
+    // Post api/city/post -- Post the data JSON format
+    [HttpPost("post")]
+    public async Task<IActionResult> AddCity(City city)
+    {
+        await context.Cities.AddAsync(city);
+        await context.SaveChangesAsync();
+        return Ok(city);
+    }
+    
+    [HttpDelete("delete/{cityId}")]
+    public async Task<IActionResult> DeleteCity(int cityId)
+    {
+        var city = await context.Cities.FindAsync(cityId);
+        context.Cities.Remove(city);
+        await context.SaveChangesAsync();
+        return Ok(city);
+    }
+    
 }
