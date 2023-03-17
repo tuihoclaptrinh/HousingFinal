5259f38bea4d7e793cc8a1ea47885d5cd7acc67c
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2023-03-17 11:22:04 +0700

    Using Repository Pattern.

---
+++ housing-back-end/Data/Repo/CityRepository.cs
@@ -0,0 +1,35 @@
+﻿using housing_back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace housing_back_end.Data.Repo;
+
+public class CityRepository: ICityRepository
+{
+    private readonly DataContext context;
+
+    public CityRepository(DataContext context)
+    {
+        this.context = context;
+    }
+    
+    public async Task<IEnumerable<City>> GetCitiesAsync()
+    {
+        return await context.Cities.ToListAsync();
+    }
+
+    public void AddCity(City city)
+    {
+        context.Cities.AddAsync(city);
+    }
+
+    public void DeteleCity(int cityId)
+    {
+        var city = context.Cities.Find(cityId);
+        context.Cities.Remove(city);
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+}
--- housing-back-end/Program.cs
+++ housing-back-end/Program.cs
@@ -1,4 +1,5 @@
 using housing_back_end.Data;
+using housing_back_end.Data.Repo;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors();
+builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbContextDefault")));
